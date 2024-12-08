2e21ff1105770ba49044b7352ba4be9c79031af5
heliosalesjr <65938536+heliosalesjr@users.noreply.github.com>
2024-12-08 20:47:39 -0300

    font ultra added

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,10 +3,10 @@
 const Hero = () => {
   return (
     <section className="bg-slate-50 text-slate-800 py-16 px-16">
-      <h1 className="text-5xl font-bold text-slate-900 mb-4">
+      <h1 className="text-5xl font-thin text-slate-900 mb-4 font-ultra">
         Curum <span className="text-green-600">Ipsum</span>
       </h1>
-      <p className="text-2xl text-slate-600 max-w-4xl">
+      <p className="text-2xl text-slate-600 max-w-4xl font-ultra">
         Curum Ipsum is placeholder text with a Brazilian twist, perfect for
         previewing layouts and mockups with a cooler, more authentic vibe.
       </p>
